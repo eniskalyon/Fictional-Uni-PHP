0cae8d990213b808fa88a5f9602e1dba35969191
eniskalyon <130925286+eniskalyon@users.noreply.github.com>
2023-10-19 15:21:20 +0100

    Add files via upload

---
+++ fictional-university-theme/404.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ */
+
+get_header(); // Load the header template
+
+
+    pageBanner();
+
+?>
+<h1 class="headline headline--medium">This post doesn't exist yet</h1>
+<p id="searchLink404" class="btn btn--blue">Would you like to search it instead?</p>
+<?php
+
+get_footer(); // Load the footer template
+?>
---
+++ fictional-university-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fcfc6d9384012a42eb10');
